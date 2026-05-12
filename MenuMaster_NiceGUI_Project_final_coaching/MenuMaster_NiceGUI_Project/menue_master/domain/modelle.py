2851e18db9ae9fe664b56fe6bbc70b0680c02729
Lukas Bläsi <lukas.blaesi@students.fhnw.ch>
2026-05-12 18:31:42 +0000

    Fehler korrigiert im modelle

--- MenuMaster_NiceGUI_Project_final_coaching/MenuMaster_NiceGUI_Project/menue_master/domain/modelle.py
+++ MenuMaster_NiceGUI_Project_final_coaching/MenuMaster_NiceGUI_Project/menue_master/domain/modelle.py
@@ -6,7 +6,7 @@ Sie sind normale Python-Klassen und unabhängig von NiceGUI und der Datenbank.
 
 from dataclasses import dataclass, field
 
-wchentage = [
+wochentage = [
     "Montag",
     "Dienstag",
     "Mittwoch",
